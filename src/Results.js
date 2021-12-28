530d5979c43fe0fc9fb9baa51304ad3cfd2081e4
Mullier <romull@amazon.com>
2021-12-28 16:05:52 -0500

    fixed build pb

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ function Results(props) {
   
   return (
     <div className={styles.main}>
-     {props.people === null? "Tyler, please fetch a character" : `We were able to fetch ${props.people.name} whose hair color is ${props.people.hair_color} and whose height is ${props.people.heigh}`}
+     {props.people === null? "Tyler, please fetch a character" : `We were able to fetch ${props.people.name} whose hair color is ${props.people.hair_color} and whose height is ${props.people.height} cm`}
     </div>
   );
 }
